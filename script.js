bc89e271c6f0eb6bc34befbf9536ebbc6d1c0b2e
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2019-09-12 12:56:37 +0100

    1.0.4.1 Improved Puzzle Start Display, Fixed Bugs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Webcam Friendly Lichess
 // @namespace    https://github.com/cyqsimon/WebcamFriendlyLichess
-// @version      1.0.4.0
+// @version      1.0.4.1
 // @icon         https://github.com/cyqsimon/WebcamFriendlyLichess/raw/master/icon.ico
 // @description  This is a simple script that moves the right-hand-side panel upwards to make space for your webcam.
 // @author       cyqsimon
@@ -99,24 +99,30 @@ var RHSPanelHeight = 55; // In percentage
         // Reduce feedback padding
         addGlobalStyle(".puzzle__feedback:not(.after) {padding: 1em 1em !important;}");
 
-        // Center view solution button
+        // Remove king icon
+        addGlobalStyle("@media (max-width: 1050px) {.puzzle__feedback .no-square {display: none;}}");
+        addGlobalStyle("@media (min-width: 1260px) and (max-width: 1330px) {.puzzle__feedback .no-square {display: none;}}");
+
+        // Center and compact view solution button
         addGlobalStyle(".puzzle__feedback .view_solution {margin: auto !important;}");
+        var viewSolutionButton = puzzleTools.getElementsByClassName("view_solution")[0].children[0];
+        viewSolutionButton.innerText = "View Solution";
 
         // Prevent feedback panel grow and horizontally arrange buttons
         addGlobalStyle("@media (min-height: 600px) {.puzzle__feedback {flex: 0 40 0rem !important; display: grid; grid-template-rows: min-content min-content; grid-template-columns: 5fr 3fr;}}");
 
         // Set vote call grid location and alignment
-        addGlobalStyle(".puzzle__feedback .after .vote_call {grid-row: 1 / 2; grid-column: 1 / 3; text-align: left;}");
+        addGlobalStyle(".puzzle__feedback.after .vote_call {grid-row: 1 / 2; grid-column: 1 / 3; text-align: left;}");
 
         // Set success & vote grid location
-        addGlobalStyle(".puzzle__feedback .after .half-top {grid-row: 2 / 3; grid-column: 1 / 2;}");
+        addGlobalStyle(".puzzle__feedback.after .half-top {grid-row: 2 / 3; grid-column: 1 / 2;}");
 
         // Correctly space success and vote
-        addGlobalStyle(".puzzle__feedback .after .complete {flex: 6 0 !important;}");
-        addGlobalStyle(".puzzle__feedback .after .vote {flex: 2 0;}");
+        addGlobalStyle(".puzzle__feedback.after .complete {flex: 6 0 !important;}");
+        addGlobalStyle(".puzzle__feedback.after .vote {flex: 2 0;}");
 
         // Set continue button grid location and compact
-        addGlobalStyle("@media (orientation: landscape) {.puzzle__feedback .after .continue {grid-row: 2 / 3; grid-column: 2 / 3; display: grid; grid-template-rows: min-content min-content; justify-items: center; padding: 0px 5px !important;}}");
+        addGlobalStyle("@media (orientation: landscape) {.puzzle__feedback.after .continue {grid-row: 2 / 3; grid-column: 2 / 3; display: grid; grid-template-rows: min-content min-content; justify-items: center; padding: 0px 5px !important;}}");
         var continueObserver = new MutationObserver(mutations => mutations.forEach(mutation =>
         {
             if(mutation.addedNodes.length != 0 && mutation.addedNodes[0].matches(".puzzle__feedback.after"))
